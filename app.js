058059b1473d14c2b23673b406eb5b07f90be7f1
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-20 13:17:15 -0700

    initate dom and state - build fetch util for requesting beanie baby data from supabase

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 /* Imports */
-
+import { getBabies } from './fetch-utils.js';
 /* Get DOM Elements */
+const babiesListEl = document.querySelector('.babies-list');
+const searchFormEl = document.querySelector('#user-form');
+const signSelectEl = document.querySelector('#sign-options');
 
 /* State */
-
+let babiesData = [];
+let signsData = [];
 /* Events */
 
 /* Display Functions */
---
+++ fetch-utils.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getBabies(sign) {
+    let { data, error } = await client.from('beanie_babies').select('*');
+
+    return data;
+}
